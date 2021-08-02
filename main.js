435832109a30e5c0cbc9ccef4096cd64fa929b74
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-08-02 20:37:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var text = "";
+var U = "";
 function DecideText(){
-    text = document.getElementById("Text").value;
+    U = document.getElementById("Text").value;
     document.getElementById("TextSelect").style = "display:none;";
     document.getElementById("Main").style = "display:contents;";
 }
 
 function Back(){
-    text = "";
+    U = "";
     document.getElementById("Main").style = "display:none;";
     document.getElementById("TextSelect").style = "display:contents;";
 }
@@ -20,16 +20,35 @@ function setup(){
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on("pose",gotPoses);
 }
+var difference;
+var nosex;
+var nosey;
 function modelLoaded(){
     console.log('No error');
+}
+function draw(){
+    console.log(U);
+
+    if(U == ""){
+        background("#FFD700");
+        fill("#C0C0C0");
+        document.getElementById("holdresult").innerHTML = "";
+    }else{
+        background("#FFD700");
+        fill("#C0C0C0");
+        text(U,nosex,nosey);
+        textSize(floor(difference/7));
+        document.getElementById("holdresult").innerHTML = "The length of the text is" + floor(difference) + "px";
+    }
+
 }
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        difference = results[0].pose.leftWrist.x - results[0].pose.rightWrist.x;
+        nosex = results[0].pose.nose.x;
+        nosey = results[0].pose.nose.y;
+        console.log(nosex);
+        console.log(nosey);
     }
-}
-function draw(){
-    
-    document.getElementById("Footer").style.color = "white";
-
 }
